0fed07b0e015dbdd6ae20299cb0eaf24caf9a56a
Rickwjx <rickwjx85@gmail.com>
2025-03-29 19:14:10 -0400

    Read id

--- inchworm_control/inchworm_control/motor_test.py
+++ inchworm_control/inchworm_control/motor_test.py
@@ -3,16 +3,18 @@ from lewansoul_servo_bus import ServoBus
 
 servo_bus = ServoBus('/dev/ttyUSB0')
 
-# print(servo_bus.id_read())
+
 
 # Move servo with ID 1 to 90 degrees in 1.0 seconds
-servo_bus.id_write(2, 1)
+# servo_bus.id_write(2, 1)
 # servo_bus.move_time_write(1, 0, 1.0, True)
 # servo_bus.move_time_write(1, 90, 1.0, True)
 # servo_1 = servo_bus.get_servo(1)
 # servo_1.move_time_write(0, 1.0, True)
 # servo_1.move_time_write(90, 1.0, True)
 # print(servo_1.pos_read())
+
+print(servo_bus.id_read())
 
 # Move servo with ID 2 to 180 degrees in 2.0 seconds
 # servo_2 = servo_bus.get_servo(2)
